6dd5abdc29794515316e6dfe49fae050cbce9f69
fuqiang1 <fuqiang1@sensetime,com>
2021-12-09 00:29:21 +0800

    获取预设

--- packages/h-cli/lib/PromptModuleAPI.js
+++ packages/h-cli/lib/PromptModuleAPI.js
@@ -2,8 +2,8 @@ class PromptModuleAPI {
   constructor(creator) {
     this.creator = creator;
   }
-  injectFeature(feture) {
-    this.creator.feturePrompt.choices.push(feature);
+  injectFeature(feature) {
+    this.creator.featurePrompt.choices.push(feature);
   }
   injectPrompt(prompt) {
     this.creator.injectedPrompts.push(prompt);
